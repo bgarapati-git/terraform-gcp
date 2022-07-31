aca60f36f4b5dce4e0dff29fc1ae54fcd0d4feb8
unknown <badari.garapati@springml.com>
2022-07-31 23:33:01 +0530

    testing

--- variables.tf
+++ variables.tf
@@ -1,11 +1,39 @@
 # Input variable definitions
 
 variable "project_id" {
-  description = "Project ID."
+  description = "GCS Project ID."
   type        = string
 }
+
 
 variable "bucket_name" {
   description = "Google Cloud Storage (GCS) bucket Name."
   type        = string
+}
+
+variable "gcs_location" {
+  description = "GCS Location."
+  type        = string
+  default     = "EU"
+}
+
+variable "force_destroy" {
+  description = "Delete all objects when deleting bucket."
+  type        = bool
+  default     = false
+}
+
+variable "storage_class" {
+  description = <<EOF
+The Storage class of the GCS bucket.
+Supported Values - STANDARD, MULTI_REGIONAL, REGIONAL, NEARLINE, COLDLINE.
+EOF
+  type        = string
+  default     = "STANDARD"
+}
+
+variable "enable_versioning" {
+  description = "Enable GCS Bucket versioning."
+  type        = bool
+  default     = false
 }
