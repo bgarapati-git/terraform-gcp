3aa7d9b8b53feedda924e6aa4bc6ab3bf6ce2858
unknown <badari.garapati@springml.com>
2022-07-31 23:30:41 +0530

    testing

--- variables.tf
+++ variables.tf
@@ -1,6 +1,11 @@
 # Input variable definitions
 
 variable "project_id" {
-  description = "Cloudrun Project ID."
+  description = "Project ID."
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Google Cloud Storage (GCS) bucket Name."
   type        = string
 }
